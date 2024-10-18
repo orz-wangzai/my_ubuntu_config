f3f729ef3b74796f05465d1e46cc0070853d261e
orz-wangzai <3139296996@qq.com>
2024-10-18 22:08:39 +0200

    now it is correct

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #！/bin/bash
 install(){
-	filepath=~/my_ubuntu_config
-	ln -s $filepath/bashrc ~/.bashrc
-	ln -s $filepath/vimrc ~/.vimrc
+	filepath=$HOME/my_ubuntu_config
+	rm -f "$HOME/.bashrc"
+	rm -f "$HOME/.vimrc"
+	ln -s "$filepath/bashrc" "$HOME/.bashrc"
+	ln -s "$filepath/vimrc" "$HOME/.vimrc"
 }
+install
